d3fd08053548a326018f93f3cba2ca222d5b7401
Hari Prasad <haririo321@gmail.com>
2024-08-12 18:57:09 +0530

    added new react project

---
+++ solace-web/solace-web/src/components/AppBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AppBar.css';
+
+function AppBar() {
+    return (
+        <div className="app-bar">
+            <h1>Solace</h1>
+            <button>Sign In</button>
+        </div>
+    );
+}
+
+export default AppBar;
